1edb9da9a886508f1c9b2564656c5d6f0e913bcc
Dan Howarth <danrhowarth@gmail.com>
2023-04-26 20:09:06 +0100

    data and model code updated

---
+++ src/train_model.py
@@ -0,0 +1,33 @@
+# Script to train machine learning model.
+
+from sklearn.model_selection import train_test_split
+from ml.data import process_data
+
+# Add the necessary imports for the starter code.
+
+# Add code to load in the data.
+
+# Optional enhancement, use K-fold cross validation instead of a train-test split.
+train, test = train_test_split(data, test_size=0.20)
+
+cat_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
+X_train, y_train, encoder, lb = process_data(
+    train, categorical_features=cat_features, label="salary", training=True
+)
+
+# Proces the test data with the process_data function.
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb,
+)
+
+
+# Train and save a model.
