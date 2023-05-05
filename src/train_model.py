c8509c0a288ebe737c1776e1727d53765762e3e9
Dan Howarth <danrhowarth@gmail.com>
2023-05-05 16:56:43 +0100

    added main block

--- src/train_model.py
+++ src/train_model.py
@@ -1,5 +1,5 @@
 # Script to train machine learning model.
-from pathlib import Path
+# from pathlib import Path
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
@@ -7,44 +7,47 @@ from joblib import dump
 from ml.data import process_data
 from ml.model import train_model, compute_model_metrics, \
     inference, assess_data_slices
+from config import path, cat_features
 
-path = Path.cwd().parent
+# path = Path.cwd().parent
+# cat_features = [
+#     "workclass",
+#     "education",
+#     "marital-status",
+#     "occupation",
+#     "relationship",
+#     "race",
+#     "sex",
+#     "native-country",
+# ]
 
 # load data and split
-data = pd.read_csv(path / 'data' / 'census.csv')
-train, test = train_test_split(data, test_size=0.20)
 
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
+if __name__ == '__main__':
+    data = pd.read_csv(path / 'data' / 'census.csv')
+    train, test = train_test_split(data, test_size=0.20)
 
-# Proces the test data with the process_data function.
-X_test, y_test, _, _ = process_data(
-    test, categorical_features=cat_features, label="salary",
-    training=False, encoder=encoder, lb=lb,
-)
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
 
-# Train and save a model.
-rf = RandomForestClassifier()
+    # Proces the test data with the process_data function.
+    X_test, y_test, _, _ = process_data(
+        test, categorical_features=cat_features, label="salary",
+        training=False, encoder=encoder, lb=lb,
+    )
 
-trained_model = train_model(rf, X_train, y_train)
+    # Train and save a model.
+    rf = RandomForestClassifier()
 
-preds = inference(trained_model, X_test)
+    trained_model = train_model(rf, X_train, y_train)
 
-precision, recall, fbeta = compute_model_metrics(y_test, preds)
+    preds = inference(trained_model, X_test)
 
-# save the model and encoder
-dump(trained_model, path / 'models' / 'random_forest.joblib')
-dump(encoder, path / 'models' / 'encoder.joblib')
+    precision, recall, fbeta = compute_model_metrics(y_test, preds)
 
-slice_data = assess_data_slices(test, encoder, lb, rf, cat_features)
+    # save the model and encoder
+    dump(trained_model, path / 'models' / 'random_forest.joblib')
+    dump(encoder, path / 'models' / 'encoder.joblib')
+
+    slice_data = assess_data_slices(test, encoder, lb, rf, cat_features)
